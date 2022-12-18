c137673fbd6a2ae538962500b5c1e6b1d0403913
FusionSid <siddheshadsv@icloud.com>
2022-12-19 00:20:25 +1300

    feat: random capy

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -9,7 +9,7 @@ from .converters import (
 from .discord_card import client, discord_card_endpoints
 from .code_execution import runcode_endpoints
 from .fun import fun_text_endpoints
-from .image import get_color_endpoints, qrcode_endpoints, ocr_endpoints, card_endpoint
+from .image import get_color_endpoints, qrcode_endpoints, ocr_endpoints, card_endpoint, capy_endpoint
 from .meme import meme_endpoints
 from .filters import color_filter_endpoint, blur_filter_endpoint
 from .minecraft import hypixel_stat_endpoints
@@ -32,5 +32,6 @@ routes = [
     redirect_endpoints,
     temp_host_endpoints,
     card_endpoint,
+    capy_endpoint,
     ocr_endpoints,
 ] + meme_endpoints
--- api/routers/image/__init__.py
+++ api/routers/image/__init__.py
@@ -2,3 +2,4 @@ from .get_colors import get_color_endpoints
 from .make_qrcode import qrcode_endpoints
 from .ocr import ocr_endpoints
 from .card import card_endpoint
+from .capy import capy_endpoint
