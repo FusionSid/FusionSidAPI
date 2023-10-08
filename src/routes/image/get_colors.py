8c87a842736b8b72538cac372966b407abb4e66d
FusionSid <siddheshadsv@icloud.com>
2023-10-08 17:25:13 +1300

    feat: add url image option for ocr and get color endpoints

--- src/routes/image/get_colors.py
+++ src/routes/image/get_colors.py
@@ -3,16 +3,13 @@ from io import BytesIO
 from colorthief import ColorThief
 from fastapi import APIRouter, UploadFile
 
+from core import get_url_image
+
 get_color_endpoints = APIRouter(tags=["Image"], prefix="/api/image")
 
 
-@get_color_endpoints.post("/get_colors/")
-async def find_colors(image: UploadFile, convert_to_hex: bool = True):
-    """Gets the colors in an image"""
-
-    image_provided_buffer = BytesIO(await image.read())
-
-    color_thief = ColorThief(image_provided_buffer)
+def get_colors_from_image(image_buffer: BytesIO, convert_to_hex: bool = True):
+    color_thief = ColorThief(image_buffer)
 
     dominant_color = list(color_thief.get_color(quality=1))
     palette: map[list[int]] = map(list, color_thief.get_palette(color_count=6))
@@ -28,3 +25,21 @@ async def find_colors(image: UploadFile, convert_to_hex: bool = True):
         "dominant_color": dominant_color_hex,
         "palette": palette_hex,
     }
+
+
+@get_color_endpoints.get("/get_colors/")
+async def find_colors_from_url(image_url: str, convert_to_hex: bool = True):
+    """Gets the colors in an image"""
+
+    image_provided_buffer = BytesIO(await get_url_image(image_url))
+
+    return get_colors_from_image(image_provided_buffer, convert_to_hex)
+
+
+@get_color_endpoints.post("/get_colors/")
+async def find_colors(image: UploadFile, convert_to_hex: bool = True):
+    """Gets the colors in an image"""
+
+    image_provided_buffer = BytesIO(await image.read())
+
+    return get_colors_from_image(image_provided_buffer, convert_to_hex)
--- src/routes/image/ocr.py
+++ src/routes/image/ocr.py
@@ -4,7 +4,19 @@ import pytesseract
 from PIL import Image
 from fastapi import APIRouter, UploadFile
 
+from core import get_url_image
+
 ocr_endpoints = APIRouter(tags=["Image"], prefix="/api/image")
+
+
+@ocr_endpoints.get("/ocr/")
+async def image_ocr_url(image_url: str):
+    """Uses Optical Character Recognition (OCR) to take an image and return the text inside it"""
+
+    image_provided_buffer = BytesIO(await get_url_image(image_url))
+    _image = Image.open(image_provided_buffer)
+
+    return pytesseract.image_to_string(_image)
 
 
 @ocr_endpoints.post("/ocr/")
